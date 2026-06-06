8764d71b9e025d409689c83fe2ceed30a7d39b8b
muscleducky <muscleducky.charcoal954@passfwd.com>
2026-06-06 16:17:38 +0100

    feat: hyprland config

---
+++ hypr/monitors.lua
@@ -0,0 +1,20 @@
+hl.monitor({
+    output   = "HDMI-A-1",
+    mode     = "1920x1080",
+    position = "auto-center-left",
+    scale    = "1",
+    transform = 1,
+})
+hl.monitor({
+    output   = "DP-2",
+    mode     = "preferred",
+    position = "0x0",
+    scale    = "1",
+})
+
+hl.monitor({
+    output   = "DP-1",
+    mode     = "1920x1080",
+    position = "1920x0",
+    scale    = "1",
+})
